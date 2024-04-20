13efbf024c43f01acf51ad8a61082eff60b7297b
noganogami <hydetkc7@gmail.com>
2024-04-21 04:11:43 +0900

    markdownファイル取得

---
+++ src/app/posts/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { getPostId } from "@/lib/posts";
+import { Box } from "@chakra-ui/react";
+
+export function generateStaticParams() {
+  return getPostId()
+}
+
+interface PostParams {
+  params: {
+    id: string,
+  };
+}
+
+export default function Post({ params }: PostParams) {
+  return (
+    <Box>
+      {params.id}
+    </Box>
+  );
+}
---
+++ src/lib/posts.ts
@@ -0,0 +1,24 @@
+import { PostType } from "@/types"
+import { readFileSync, readdirSync } from "fs"
+import matter from "gray-matter"
+import { join } from "path"
+
+const BASE_DIR = join(process.cwd(), '/src/posts')
+
+export const getPostData = (id: string): PostType => {
+  const filePath = join(BASE_DIR, `${id}.md`)
+  const file = readFileSync(filePath)
+  const { data, content } = matter(file)
+  return {
+    id,
+    ...data as {title: string, date: string},
+    content,
+  }
+}
+
+export const getPostId = () => {
+  const files = readdirSync(BASE_DIR)
+  const ids = files.map((filename) =>  {return { id: filename.replace(/\.md$/, '') }})
+
+  return ids
+}
